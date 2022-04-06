de60b6ecf17338faeb90e3c98138cef99695a96c
benedictusdps <benedictusdps@gmail.com>
2022-04-06 17:51:56 +0700

    Add files from upload

--- 02_FunctionsArraysLoops/MagicEightBall.js
+++ 02_FunctionsArraysLoops/MagicEightBall.js
@@ -21,21 +21,21 @@ let randomNumber = Math.floor(Math.random() * 8);
 let eightBall = "";
 
 // Control flow for the Magic Eight Ball reply
-if (randomNumber == 0) {
+if (randomNumber === 0) {
   eightBall = "It is certain";
-} else if (randomNumber == 1) {
+} else if (randomNumber === 1) {
   eightBall = "It is decidedly so";
-} else if (randomNumber == 2) {
+} else if (randomNumber === 2) {
   eightBall = "Reply hazy try again";
-} else if (randomNumber == 3) {
+} else if (randomNumber === 3) {
   eightBall = "Cannot predict now";
-} else if (randomNumber == 4) {
+} else if (randomNumber === 4) {
   eightBall = "Do not count on it";
-} else if (randomNumber == 5) {
+} else if (randomNumber === 5) {
   eightBall = "My sources say no";
-} else if (randomNumber == 6) {
+} else if (randomNumber === 6) {
   eightBall = "Outlook not so good";
-} else if (randomNumber == 7) {
+} else if (randomNumber === 7) {
   eightBall = "Signs point to yes";
 }
 
